0776a6753c206378063b2232b1be49c612bc8ca4
joanna <joanna@strawberry.(none)>
2012-06-08 16:45:24 +1000

    update.

--- pipeline/pipeline_wrapper.sh
+++ pipeline/pipeline_wrapper.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Usage: ./pipeline_wrapper.sh sample_file.dat
+# Usage: ./pipeline_wrapper.sh /directory/path/sample_file.dat
 
 # FILE is the full name of file to be used for models, ex. sample_file.dat
 # FILENAME is the file name without extension, ex. sample_file
-FILE=`basename $1`
-FILENAME=${FILE%.*}
+#FILE=`basename $1`
+#FILENAME=${FILE%.*}
 
-FIRST=`qsub -v file=$FILE preprocessor.pbs`
+FILE_PATH = $1
+
+FIRST=`qsub -v file=$FILE_PATH preprocessor.pbs`
 echo $FIRST
-SECOND=`qsub -W depend=afterok:$FIRST -t 1-9 -v filename=$FILENAME dammif.pbs`
+SECOND=`qsub -W depend=afterok:$FIRST -t 1-9 -v filename=$FILE_PATH dammif.pbs`
 echo $SECOND
-THIRD=`qsub -W depend=afterokarray:$SECOND -v filename=$FILENAME damclust.pbs`
+THIRD=`qsub -W depend=afterokarray:$SECOND -v filename=$FILE_PATH damclust.pbs`
 echo $THIRD
 #FOURTH=`qsub -W depend=afterok:$THIRD postprocessor.pbs`
 #echo $FOURTH
--- pipeline/preprocessor.py
+++ pipeline/preprocessor.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 
-import sys
+
 import getopt
-import subprocess
 import shutil
+import subprocess
+import sys
 
 
 def main():
