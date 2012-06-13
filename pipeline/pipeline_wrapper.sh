b811940af2e9947d5850f578da7c2bece96cd9c4
joanna <joanna@strawberry.(none)>
2012-06-13 15:38:52 +1000

    updated scripts to enalbe separate input and output directories.

--- pipeline/pipeline_wrapper.sh
+++ pipeline/pipeline_wrapper.sh
@@ -2,22 +2,29 @@
 
 #--------- pipeline_wrapper.sh ------------------#
 #
-# Usage: $ ./pipeline_wrapper.sh /full/path/sample_file.dat
+# Usage:
+# 
+#     $ ./pipeline_wrapper.sh DAT_FILE OUTPUT_PATH
+# 
+# DAT_FILE:
+#
+#     It is the full path of your SAS experimental data file to be used for models.
+#     example: /home/saxswaxs/ASync011_scratch/pipeline_data/test_user_epn/test_experiment/avg/sample.dat
+#
+# OUTPUT_PATH: 
+#
+#     It is the full directory path for all output files generated during pipeline modelling. 
+#     example:/home/saxswaxs/ASync011_scratch/pipeline_data/test_user_epn/test_experiment/analysis
 #
 
-
-# FILE is the full name of file to be used for models, ex. sample_file.dat
-# FILENAME is the file name without extension, ex. sample_file
-#FILE=`basename $1`
-#FILENAME=${FILE%.*}
-
 DAT_FILE=$1
+OUTPUT_PATH=$2
 
-FIRST=`qsub -v dat_file=$DAT_FILE preprocessor.pbs`
+FIRST=`qsub -v dat_file=$DAT_FILE,output_path=$OUTPUT_PATH preprocessor.pbs`
 echo $FIRST
-SECOND=`qsub -W depend=afterok:$FIRST -t 1-9 -v dat_file=$DAT_FILE dammif.pbs`
+SECOND=`qsub -W depend=afterok:$FIRST -t 1-9 -v dat_file=$DAT_FILE,output_path=$OUTPUT_PATH dammif.pbs`
 echo $SECOND
-THIRD=`qsub -W depend=afterokarray:$SECOND -v dat_file=$DAT_FILE damclust.pbs`
+THIRD=`qsub -W depend=afterokarray:$SECOND -v dat_file=$DAT_FILE,output_path=$OUTPUT_PATH damclust.pbs`
 echo $THIRD
 #FOURTH=`qsub -W depend=afterok:$THIRD postprocessor.pbs`
 #echo $FOURTH
--- pipeline/preprocessor.py
+++ pipeline/preprocessor.py
@@ -11,7 +11,7 @@ def main():
     datfile = ""
     
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "f:", ["file"])
+        opts, args = getopt.getopt(sys.argv[1:], "f:o:", ["file", "output"])
     except getopt.GetoptError, err:
         # print help information and exit:
         print str(err) # will print something like "option -a not recognized"
@@ -22,15 +22,17 @@ def main():
     for o, a in opts:
         if o in ("-f", "--file"):
             datfile = str(a)
+        if o in ("-o", "--output"):
+            output_path = str(a)
 
     if not datfile.endswith('.dat'):
         print "ERROR: *.dat file is expected as input file."
         sys.exit(2)
 
-    processDatFile(datfile)
+    processDatFile(datfile, output_path)
 
 
-def processDatFile(datfile):
+def processDatFile(datfile, output_path):
   
     print '#--------- autorg ------------------#'
     process = subprocess.Popen(['autorg', '-f', 'ssv', datfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -46,7 +48,10 @@ def processDatFile(datfile):
         skip = skip - 1
     except ValueError:
         print "Error happened when converting skip value into integer."
-    outfile = datfile.replace('.dat', '.out')
+    file = datfile.split('/')[-1]
+    if file.endswith('.dat'):
+        filename = datfile[:-4] #example: filename="sample" if input file is /input_path/sample.dat
+    outfile = output_path + filename + '.out'
     process = subprocess.Popen(['datgnom', '-r', str(rg), '-s', str(skip), '-o', outfile, datfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (output, errorOutput) = process.communicate()
     # Output file: gnom .out file
@@ -59,12 +64,11 @@ def processDatFile(datfile):
   
     print '#--------- dammif in slow mode -----#'
     # copy .out file
-    if datfile.endswith('.dat'):
-        filename = datfile[:-4] #example: filename = "/path/sample" if input file is /path/sample.dat
-    dammif_outfile = str(filename) + "_dammif.out" #example: dammif_outfile = "/path/sample_dammif.out" if input file is /path/sample.dat
+    
+    dammif_outfile = output_path + filename + "_dammif.out" #example: dammif_outfile="/output_path/sample_dammif.out" if input file is /input_path/sample.dat
     shutil.copyfile(outfile, dammif_outfile)
     # dammif modelling
-    dammif_slow_outfile_prefix = str(filename) + "_0" #example: dammif_slow_outfile_prefix = "/path/sample_0" if input file is /path/sample.dat
+    dammif_slow_outfile_prefix = output_path + filename + "_0" #example: dammif_slow_outfile_prefix="/output_path/sample_0" if input file is /input_path/sample.dat
     process = subprocess.Popen(['dammif', '--prefix=%s' % dammif_slow_outfile_prefix, '--mode=slow', '--symmetry=P1', '--unit=n', dammif_outfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     (output, errorOutput) = process.communicate()
     # remove random seed line from "/path/sample_0.in" output file if input file is /path/sample.dat
@@ -81,9 +85,10 @@ def processDatFile(datfile):
 
 
 def usage():
-    print 'Usage: %s [OPTIONS] -f filename.dat \n' % (sys.argv[0])
+    print 'Usage: %s [OPTIONS] -f /full/path/filename.dat -o /output/full/path/ \n' % (sys.argv[0])
     print '''
--f --file      DAT file name which is the full name of file to be used for models.
+-f --file      the full path of your SAS experimental data file to be used for models.
+-o --output    the full directory path for all output files generated during pipeline modelling. 
 
 
 '''
