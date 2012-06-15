2f85bf2a56e63c5fda89e739895cd0ce46f98d63
Joanna Huang <joanna.huitzu.huang@gmail.com>
2012-06-15 12:24:29 +1000

    updated workflow.sh

--- pipeline/workflow.sh
+++ pipeline/workflow.sh
@@ -4,16 +4,18 @@
 PROD_USER=joannah
 PROD_HOST=as-saxs-dev.synchrotron.org.au
 
-PROD_SOURCE_CODE_HOME=/gpfs/M1Home/projects/ASync011/as-saxs/pipeline
+PROD_SOURCE_CODE_HOME=/home/joannah/as-saxs/pipeline
 PROD_DATA_HOME=/home/joannah/production_data
 PROD_PIPELINE_HARVEST=pipeline_harvest.sh
 
 PROD_USER_EPN=joannah_epn
 PROD_USER_EXP=joannah_exp
 PROD_USER_DAT_FILE=sample.dat
+PROD_PIPELINE_INPUT_DIR=avg
 PROD_PIPELINE_OUTPUT_DIR=analysis
 
 PROD_SSH_ACCESS="$PROD_USER"@"$PROD_HOST"
+PROD_PIPELINE_SCP_FROM="$PROD_DATA_HOME"/"$PROD_USER_EPN"/"$PROD_USER_EXP"/"$PROD_PIPELINE_INPUT_DIR"/
 PROD_PIPELINE_SCP_DEST="$PROD_DATA_HOME"/"$PROD_USER_EPN"/"$PROD_USER_EXP"/"$PROD_PIPELINE_OUTPUT_DIR"/
 PROD_PIPELINE_HARVEST_PATH="$PROD_SOURCE_CODE_HOME"/"$PROD_PIPELINE_HARVEST"
 
@@ -25,8 +27,8 @@ PIPELINE_SOURCE_CODE_HOME=/gpfs/M1Home/projects/ASync011/as-saxs/pipeline
 PIPELINE_DATA_HOME=/gpfs/M1Scratch/ASync011/pipeline_data
 PIPELINE_WRAPPER=pipeline_wrapper.sh
 
-PIPELINE_INPUTPUT_DIR=avg
-PIPELINE_OUTPUT_DIR=analysis
+PIPELINE_INPUT_DIR="$PROD_PIPELINE_INPUT_DIR"
+PIPELINE_OUTPUT_DIR="$PROD_PIPELINE_OUTPUT_DIR"
 
 PIPELINE_USER_EXP_DIR="$PIPELINE_DATA_HOME"/"$PROD_USER_EPN"/"$PROD_USER_EXP"
 PIPELINE_USER_INPUT_DIR="$PIPELINE_USER_EXP_DIR"/"$PIPELINE_INPUT_DIR"
@@ -41,7 +43,7 @@ ssh "$MASSIVE_USER"@"$MASSIVE_HOST" mkdir -p "$PIPELINE_USER_OUTPUT_DIR"
 
 #---------- Cope user's experimental data file --------------------------------#
 # Copy a local data file on production to a remote massive host 
-scp "$PROD_USER_DAT_FILE" "$MASSIVE_USER"@"$MASSIVE_HOST":"$PIPELINE_USER_INPUT_DIR"/"$PROD_USER_DAT_FILE"
+scp "$PROD_PIPELINE_SCP_FROM"/"$PROD_USER_DAT_FILE" "$MASSIVE_USER"@"$MASSIVE_HOST":"$PIPELINE_USER_INPUT_DIR"/"$PROD_USER_DAT_FILE"
 
 #---------- Pipeline modelling ------------------------------------------------#
 # Submit pipeline process jobs on to massive cluster
